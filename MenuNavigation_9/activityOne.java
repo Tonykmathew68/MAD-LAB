62c3a498800571a3587777b7e50f1efb04509e7c
Tonykmathew68 <chn24mc068@ceconline.edu>
2025-09-16 20:29:28 +0530

    Create activityOne.java

---
+++ MenuNavigation_9/activityOne.java
@@ -0,0 +1,10 @@
+package com.example.optionsmenuapp; 
+import android.os.Bundle; 
+import androidx.appcompat.app.AppCompatActivity;  
+public class ActivityOne extends AppCompatActivity {
+  @Override 
+    protected void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.activity_one);
+    } 
+}
